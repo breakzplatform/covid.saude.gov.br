9b5e83c90f67a1e86806c452e46c0fd51089fc20
Gabriel Carneiro <gabriel.carneiro97@live.com>
2020-06-09 08:59:41 -0300

    adicionei a exposição da API do governo via Puppeteer

---
+++ api/govApi/[govApi].ts
@@ -0,0 +1,52 @@
+import { NowRequest, NowResponse } from '@vercel/node';
+import pptr from 'puppeteer-core';
+import chrome from 'chrome-aws-lambda';
+
+const apisConhecidas = [
+  'PortalGeral',
+  'PortalGeralApi',
+  'PortalRegiao',
+  'PortalEstado',
+  'PortalSintese',
+];
+
+async function gov(path: string): Promise<unknown> {
+  const browser = await pptr.launch({
+    args: chrome.args,
+    executablePath: await chrome.executablePath,
+    headless: chrome.headless
+  });
+  const page = await browser.newPage();
+
+  return new Promise(async (resolve) => {
+    page.on('response', async (response) => {
+      if (response.url().endsWith(path)) {
+        try {
+          const data = await response.json();
+          resolve(data);
+          browser.close();
+        } catch (e) {}
+      }
+    });
+
+    await page.goto('https://covid.saude.gov.br');
+  });
+}
+
+export default async (req: NowRequest, res: NowResponse) => {
+  const {
+    query: { govApi }
+  } = req;
+
+  console.log(govApi);
+
+  const res204 = () => res.status(204).send({});
+
+  if (Array.isArray(govApi)) return res204();
+  if (!apisConhecidas.includes(govApi)) return res204();
+
+  const data = await gov(govApi);
+
+  res.send(data);
+
+}
